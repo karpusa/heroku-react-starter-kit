e3a4b3aae2191dfc522e1b89a4576254c17fe6bd
Vitalijs Karpusa <vkarpusha@gmail.com>
2017-02-22 13:18:27 +0200

    fix: class name

--- src/containers/video_detail.js
+++ src/containers/video_detail.js
@@ -13,7 +13,7 @@ class VideoDetail extends Component {
     const url = `https://www.youtube.com/embed/${videoId}`;
 
     return (
-      <div className={styles.root + ` col-md-8`}>
+      <div className="col-md-8">
         <div className="embed-responsive embed-responsive-16by9">
           <iframe className="embed-responsive-item" src={url}></iframe>
         </div>

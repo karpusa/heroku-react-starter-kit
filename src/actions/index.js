d89fa5b5165a857c37c030e47f70f9b5b82d8e04
Vitalijs Karpusa <vkarpusha@gmail.com>
2017-02-22 12:36:41 +0200

    feat: enable mock data with url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,25 @@
 import axios from 'axios';
-import mock from '../mock.json';
-//console.log(mock);
 
 const API_KEY = 'AIzaSyD22bSJa6989EsRhSr2TyG4RYkyh84amnc';
 const API_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 export const SEARCH_VIDEO = 'SEARCH_VIDEO';
 export const SELECT_VIDEO = 'SELECT_VIDEO';
+
+export function searchMock(data) {
+  return (dispatch) => {
+    dispatch(
+      {
+        type: SEARCH_VIDEO,
+        payload: data.videos
+      }
+    );
+
+    if (data.videos[0]) {
+      dispatch(selectVideo(data.videos[0].id.videoId));
+    }
+  }
+}
 
 export function searchVideo(term) {
   return (dispatch) => {
@@ -15,34 +28,24 @@ export function searchVideo(term) {
       key: API_KEY,
       q: term,
       type: 'video',
-      maxResults: 50
+      maxResults: 10
     };
 
-    dispatch(
-      {
-        type: SEARCH_VIDEO,
-        payload: mock.videos
-      }
-    );
-    if (mock.videos[0]) {
-      dispatch(selectVideo(mock.videos[0].id.videoId));
-    }
-
-    // axios.get(API_URL, { params: params })
-    //   .then(function(response) {
-    //     dispatch(
-    //       {
-    //         type: SEARCH_VIDEO,
-    //         payload: response.data.items
-    //       }
-    //     );
-    //     if (response.data.items[0]) {
-    //       dispatch(selectVideo(response.data.items[0].id.videoId));
-    //     }
-    //   })
-    //   .catch(function(error) {
-    //     console.error(error);
-    //   });
+    axios.get(API_URL, { params: params })
+      .then(function(response) {
+        dispatch(
+          {
+            type: SEARCH_VIDEO,
+            payload: response.data.items
+          }
+        );
+        if (response.data.items[0]) {
+          dispatch(selectVideo(response.data.items[0].id.videoId));
+        }
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
   };
 }
 
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { searchVideo } from '../actions/index';
+import { searchVideo, searchMock } from '../actions/index';
 import SearchBar from './search_bar';
 import VideoList from './video_list';
 import VideoDetail from './video_detail';
 import Perf from 'react-addons-perf';
 import ProfTool from '../helpers/profTool';
+import mock from '../mock.json';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    props.searchVideo('Simon\'s Cat');
+    if (window.location.search.indexOf('mock')===-1) {
+      props.searchVideo('Simon\'s Cat');
+    } else {
+      props.searchMock(mock);
+    }
   }
 
   render() {
@@ -28,7 +33,7 @@ class App extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ searchVideo }, dispatch);
+  return bindActionCreators({ searchVideo, searchMock }, dispatch);
 }
 
 export default connect(null, mapDispatchToProps)(App);

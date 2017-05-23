9577ff9dd88024101785cd41f2fd0943a306969d
Vitalijs Karpusa <vkarpusha@gmail.com>
2017-05-24 00:12:52 +0300

    chore: comment code

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ app.set('port', (process.env.PORT || 8000));
 
 app.use(express.static(__dirname));
 
-// app.get('mock.json', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'src/mock.json'));
-// });
-
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
@@ -18,15 +14,15 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
-const jsonServer = require('json-server');
-const mockDB = require('./mock/db.js');
-const server = jsonServer.create();
-const router = jsonServer.router(mockDB());
-const middlewares = jsonServer.defaults();
-const port = 3000;
-server.use(middlewares);
-server.use(router);
-server.listen(port, function () {
-  console.log('JSON Server is running')
-});
+// Heroku not supported multiple ports
+// const jsonServer = require('json-server');
+// const mockDB = require('./mock/db.js');
+// const server = jsonServer.create();
+// const router = jsonServer.router(mockDB());
+// const middlewares = jsonServer.defaults();
+// const port = 3000;
+// server.use(middlewares);
+// server.use(router);
+// server.listen(port, function () {
+//   console.log('JSON Server is running')
+// });
